b0e1467f533a192412d703c661db53346b4a70c9
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-06-01 16:29:04 +0300

    Initial commit

---
+++ src/main/java/xyz/vegaone/userProvisioning/UserProvisioningApplication.java
@@ -0,0 +1,12 @@
+package xyz.vegaone.userProvisioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProvisioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProvisioningApplication.class, args);
+	}
+}
---
+++ src/test/java/xyz/vegaone/userProvisioning/UserProvisioningApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.vegaone.userProvisioning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserProvisioningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
