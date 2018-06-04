5e0aa124a6018a03d223af01097de65230a13c88
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-06-04 19:03:15 +0300

    Added @EnableEurekaClient

--- src/main/java/xyz/vegaone/userProvisioning/UserProvisioningApplication.java
+++ src/main/java/xyz/vegaone/userProvisioning/UserProvisioningApplication.java
@@ -2,7 +2,9 @@ package xyz.vegaone.userProvisioning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserProvisioningApplication {
 
